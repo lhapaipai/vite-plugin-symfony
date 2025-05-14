8243b2eec95ac904e4f4636ab34bb748d2de7e04
Hugues Tavernier <hugues.tavernier@protonmail.com>
2025-05-15 00:08:19 +0200

    remove hardcoded ux metadatas

--- scripts/pre-dev.ts
+++
@@ -1,7 +0,0 @@
-import { cp } from "fs/promises";
-import { resolve } from "path";
-
-const assetsDir = resolve(import.meta.dirname, "ux-assets");
-const nodeModules = resolve(import.meta.dirname, "../node_modules");
-
-await cp(assetsDir, nodeModules, { recursive: true });
